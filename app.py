264f0f51c7b6171681d30ecfcc7ef4ea05f8c11e
hypergalois <jose.delgado2@live.u-tad.com>
2024-06-07 17:59:49 +0200

    Cleaning up + Readme

--- app.py
+++ app.py
@@ -7,42 +7,51 @@ from utils import transform_data
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-st.title('Predecir Abandono de Clientes 🚀')
-st.write('¡Haz clic en Predecir para determinar si es probable que tu cliente abandone!')
+# If anyone has trouble with the pickle version:
+# https://github.com/scikit-learn/scikit-learn/issues/28666
 
-with open('schema.json', 'r') as f:
+st.title("Predecir Abandono de Clientes 🚀")
+st.write(
+    "¡Haz clic en Predecir para determinar si es probable que tu cliente abandone!"
+)
+
+with open("./data/schema.json", "r") as f:
     schema = json.load(f)
 # st.write(schema)
 
-column_order_in = list(schema['column_info'].keys())[:-1]
-column_order_out = list(schema['transformed_columns']['transformed_columns'])
+column_order_in = list(schema["column_info"].keys())[:-1]
+column_order_out = list(schema["transformed_columns"]["transformed_columns"])
 # st.write(column_order_out)
 
 # Sidebar Section
 st.sidebar.info("¡Actualiza estas características!")
 # Collect the input features
 options = {}
-for column, column_properties in schema['column_info'].items():
-    if column == 'churn':
+for column, column_properties in schema["column_info"].items():
+    if column == "churn":
         pass
-    elif column_properties['dtype'] == 'int64' or column_properties['dtype'] == 'float64':
-        min_val, max_val = column_properties['values']
-        data_type = column_properties['dtype']
+    elif (
+        column_properties["dtype"] == "int64" or column_properties["dtype"] == "float64"
+    ):
+        min_val, max_val = column_properties["values"]
+        data_type = column_properties["dtype"]
 
         feature_mean = (min_val + max_val) / 2
-        if data_type == 'int64':
+        if data_type == "int64":
             feature_mean = int(feature_mean)
 
-        options[column] = st.sidebar.slider(column, min_val, max_val, value=feature_mean)
-    
-    elif column_properties['dtype'] == 'object':
-        options[column] = st.sidebar.selectbox(column, column_properties['values'])
+        options[column] = st.sidebar.slider(
+            column, min_val, max_val, value=feature_mean
+        )
+
+    elif column_properties["dtype"] == "object":
+        options[column] = st.sidebar.selectbox(column, column_properties["values"])
 
 # Load model and encoder
-with open('xg.pkl', 'rb') as f:
+with open("./models/xg.pkl", "rb") as f:
     model = pickle.load(f)
 
-with open('encoder.pkl', 'rb') as f:
+with open("./models/encoder.pkl", "rb") as f:
     onehot = pickle.load(f)
 
 mean_eve_mins = 200.29
@@ -50,53 +59,56 @@ mean_eve_mins = 200.29
 # st.write(options)
 
 # Make our predictions
-if st.button('Predecir'):
+if st.button("Predecir"):
     # Convert options to df
     scoring_data = pd.Series(options).to_frame().T
     scoring_data = scoring_data[column_order_in]
-    
 
     # Check datatypes
-    for column, column_properties in schema['column_info'].items():
-        if column != 'churn':
-            dtype = column_properties['dtype']
+    for column, column_properties in schema["column_info"].items():
+        if column != "churn":
+            dtype = column_properties["dtype"]
             scoring_data[column] = scoring_data[column].astype(dtype)
 
     # st.write(scoring_data)
 
     # Apply data transformations
-    scoring_sample = transform_data(scoring_data, column_order_out, mean_eve_mins, onehot)
+    scoring_sample = transform_data(
+        scoring_data, column_order_out, mean_eve_mins, onehot
+    )
     # st.write(scoring_sample)
 
     # Render Predictions
     prediction = model.predict(scoring_sample)
-    st.write('Resultado:')
+    st.write("Resultado:")
 
     if prediction[0] == 0:  # We use prediction[0] because prediction is an array
-        st.write('¡El cliente se queda! 🔥🤩')
+        st.write("¡El cliente se queda! 🔥🤩")
     else:
-        st.write('El cliente se va... 💣😥')
+        st.write("El cliente se va... 💣😥")
 
-    st.write('Detalles Cliente')
+    st.write("Detalles Cliente")
     st.write(options)
 
 
 try:
     historical = pd.Series(options).to_frame().T
-    historical['prediction'] = prediction
-    if os.path.isfile('historical_data.csv'):
-        historical.to_csv('historical_data.csv', mode='a', header=False, index=False)
+    historical["prediction"] = prediction
+    if os.path.isfile("./data/historical_data.csv"):
+        historical.to_csv(
+            "./data/historical_data.csv", mode="a", header=False, index=False
+        )
     else:
-        historical.to_csv('historical_data.csv', header=True, index=False)
+        historical.to_csv("./data/historical_data.csv", header=True, index=False)
 except Exception as e:
     pass
 
-st.header('Predicciones Históricas')
-if os.path.isfile('historical_data.csv'):   
-    hist = pd.read_csv('historical_data.csv')
+st.header("Predicciones Históricas")
+if os.path.isfile("./data/historical_data.csv"):
+    hist = pd.read_csv("./data/historical_data.csv")
     st.dataframe(hist)
     fig, ax = plt.subplots()
-    sns.countplot(x='prediction', data=hist, ax=ax).set_title('Predicciones Históricas')
+    sns.countplot(x="prediction", data=hist, ax=ax).set_title("Predicciones Históricas")
     st.pyplot(fig)
 else:
-    st.write('Sin datos históricos todavía.')
+    st.write("Sin datos históricos todavía.")
